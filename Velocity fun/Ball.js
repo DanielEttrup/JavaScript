f6cdfd78422860e8b2edaae83888a8b3b169c9df
Daniel Ettrup <daniel.ettru@live.dk>
2018-10-31 20:57:22 +0100

    ready for more physic

--- Velocity fun/Ball.js
+++ Velocity fun/Ball.js
@@ -1,9 +1,25 @@
 class Ball {
 	
-	constructor(){
+	constructor(mass){
 		this.location = new createVector(width/2,height/2);
 		this.velocity = new createVector(0,0);
 		this.acceleration = new createVector(0,0);
+		this.mass = mass;
 	}
 
-}
+	update(){
+		this.velocity.add(this.acceleration);
+		this.location.add(this.velocity);
+	}
+	gravity(x,y){
+		var gravityVector = new createVector(x,y);
+		gravityVector.sub(this.location);
+		gravityVector.setMag(0.08);
+		this.acceleration = (gravityVector);
+	}
+	addForce(x,y){
+		var forceVector = new createVector(x,y);
+		this.velocity.add(forceVector);
+	}
+
+}			
