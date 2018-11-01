5b5bf816ce57eb742653109891d3123b6d56bdfd
Daniel Ettrup <daniel.ettrup@live.dk>
2018-11-01 13:20:03 +0100

    added dist to acc megatude calculation

--- Velocity fun/Ball.js
+++ Velocity fun/Ball.js
@@ -13,8 +13,10 @@ class Ball {
 	}
 	gravity(x,y){
 		var gravityVector = new createVector(x,y);
+		var dist = Math.sqrt((Math.abs(gravityVector.x-this.location.x)^2) + (Math.abs(gravityVector.y-this.location.y)^2));
+		map(dist,0,100,0.1,0.01); // 
 		gravityVector.sub(this.location);
-		gravityVector.setMag(0.08);
+		gravityVector.setMag(dist);
 		this.acceleration = (gravityVector);
 	}
 	addForce(x,y){
