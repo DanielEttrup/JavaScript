5932bc97027f82c62de7fd964f8820680cebc36a
Daniel Ettrup <daniel.ettrup@live.dk>
2018-11-13 08:18:44 +0100

    change corrodinatsystem to v1.0

--- CorrodinatSystem/sketch.js
+++ CorrodinatSystem/sketch.js
@@ -4,11 +4,17 @@ function setup() {
 	createCanvas(400,400);
 
 	background(0); 
-	corrodinatSystem = new CorrodinatSystem(20,40);
+	corrodinatSystem = new CorrodinatSystem(40, 0, 400, 400);
 }
 
 function draw() {
-	background(0); 
- 	corrodinatSystem.Render();
- 	translate(height/2,width/2);
+	background(51); 
+ 	corrodinatSystem.render();
+ 	stroke(255,0,0);
+ 	strokeWeight(10);
+ 	corrodinatSystem.renderPoints();
+
+}
+function mousePressed(){
+	corrodinatSystem.addVector({x: mouseX, y: mouseY});
 }
